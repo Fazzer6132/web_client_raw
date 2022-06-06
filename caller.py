d049b3744fee41a404402767d5f78d348b9e673c
Fazzer6132 <50322696+Fazzer6132@users.noreply.github.com>
2022-06-06 21:01:46 +0300

    Api call gets written to base

--- caller.py
+++ caller.py
@@ -4,4 +4,4 @@ url = "https://www.openexchangerates.org"
 app_id = "f4ac58b20ec54e19b5ac494b52c7c05c"
 
 WClient = web_client.WebClient(url, app_id)
-print(WClient.request_rate("UAH"))
+WClient.write_to_base()
--- web_client.py
+++ web_client.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import psycopg2
 
 
 class WebClient:
@@ -7,7 +8,7 @@ class WebClient:
         self.url = url
         self.app_id = app_id
 
-    def __request_info(self):
+    def request_info(self):
         response = requests.get(f"{self.url}/api/latest.json?app_id={self.app_id}")
         json_data = json.loads(response.text)
         return json_data
@@ -16,3 +17,21 @@ class WebClient:
         json_data = self.__request_info()
         rate = json_data["rates"][key]
         return rate
+
+    def write_to_base(self):
+        response = requests.get(f"{self.url}/api/latest.json?app_id={self.app_id}")
+        json_data = json.loads(response.text)
+        rates = json_data["rates"]
+        UAH = rates["UAH"]
+        GBP = rates["GBP"]
+        PLN = rates["PLN"]
+        EUR = rates["EUR"]
+        timestamp = json_data["timestamp"]
+        conn = psycopg2.connect(host='localhost', database='scraper_db', user='scraper_user', password='qwerty')
+        sql = f"INSERT INTO json_base \
+              VALUES ({UAH}, {GBP}, {PLN}, {EUR}, {timestamp})"
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
+        cur.close()
+        conn.close()
